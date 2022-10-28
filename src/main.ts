ea051f770f667eaff07ad52ffb9f0dc3cbc924c8
Ucrash <gducrash@gmail.com>
2022-10-28 21:32:28 +0300

    modularize

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,19 @@
-import test from './test/test';
+import Grammar, { either } from "./grammar/grammar";
+import GrammarRule, { 
+    GrammarAtom, GrammarBinaryLoop, 
+    GrammarBlockLoop, GrammarEither 
+} from "./grammar/rule";
 
-test;
+import ASTNode from "./classes/ASTNode";
+import ParseError from "./classes/ParseError";
+import Token from "./classes/Token";
+import Parser from "./parser/parser";
+
+export {
+    Grammar, either,
+    GrammarRule, GrammarAtom,
+    GrammarBinaryLoop, GrammarBlockLoop, 
+    GrammarEither, 
+
+    ASTNode, ParseError, Token, Parser
+}
